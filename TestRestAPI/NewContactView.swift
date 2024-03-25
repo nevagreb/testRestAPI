0b759718b4be7a08f72c9f3e561237dc2f41821d
Kristina Grebneva <kristina.o.grebneva@gmail.com>
2024-03-25 14:12:47 +0100

    Added deleting functionality and NewContactView

---
+++ TestRestAPI/NewContactView.swift
@@ -0,0 +1,18 @@
+//
+//  NewContactView.swift
+//  TestRestAPI
+//
+//  Created by Kristina Grebneva on 25.03.2024.
+//
+
+import SwiftUI
+
+struct NewContactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewContactView()
+}
